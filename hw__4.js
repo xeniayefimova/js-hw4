00b665ec8ea409f6750f754ab112f27f8c3d602e
xeniayefimova <162689466+xeniayefimova@users.noreply.github.com>
2024-04-29 10:28:31 +0200

    Add files via upload

---
+++ hw__4.js
@@ -0,0 +1,32 @@
+//1. Ініціалізація числових змінних
+var integerNumber 
+integerNumber = 61894
+console.log(integerNumber);
+
+var floatingPointNumber 
+floatingPointNumber = 431.406 
+console.log(floatingPointNumber);
+
+//2.BigInt
+
+var largeNumber = 1_000_000 
+console.log(largeNumber);
+
+//3. Експоненціальний запис чисел
+
+var cientificNotation
+cientificNotation = 1.23e5 // цей запис означає  1.23 * 10^5 = 123000
+console.log(cientificNotation); 
+
+//4. Використання об'єкту Math для генерації випадкових чисел:
+
+var randomNumber
+randomNumber = Math.random() 
+console.log(randomNumber); //0.05805877083739075
+
+//5. Особливості чисел у JavaScript:
+
+const firstDecimal = 0.11
+const secondDecimal = 0.12
+const sum = firstDecimal + secondDecimal 
+console.log(sum);  // 0.22999999999999998
